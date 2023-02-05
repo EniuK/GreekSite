c2444a0cd60a5d7f9bbf06166a1733559338f566
EniuK <108581363+EniuK@users.noreply.github.com>
2023-02-05 11:30:29 +0100

    first menu added

---
+++ greece/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="menu">
+        <div className="logo">LOGO</div>
+        <div className="katheto">katheto</div>
+        <div className="helpxd"></div>
+        <div className="socials">
+          <div className="ig">ig</div>
+          <div className="fb">fb</div>
+          <div className="tw">tw</div>
+        </div>
+      </div>
+      <div className="undermenu">
+        <div className="maskin">
+          Μαζική αναγνωσισιμότητα για την επιχείρηση σου
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
